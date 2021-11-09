d96abb174dda579e1c48812e2d0716d02699ff9b
kevin1547 <93572270+kevin1547@users.noreply.github.com>
2021-11-09 16:31:42 +0800

    Add files via upload

---
+++ NKUST1/src/BMI.java
@@ -0,0 +1,8 @@
+public class BMI {
+    public static void main(String[] args) {
+        double height = 1.75;
+        int weight = 72;
+        double BMI = weight / height^2;
+        System.out.println( BMI );
+    }
+}
---
+++ NKUST1/src/Variable.java
@@ -0,0 +1,6 @@
+public class Variable {
+    public static void main(String[] args) {
+        String myName = "Kevin";
+        System.out.println("Hello " + myName);
+    }
+}
