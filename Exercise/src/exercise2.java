dccf02d6885a63c751280dbf81d979425415c05a
kevin1547 <c110181143@nkust.edu.tw>
2021-11-18 18:06:52 +0800

    11/16 work

--- Exercise/src/exercise2.java
+++ Exercise/src/exercise2.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class exercise2 {
-}
+public class exercise2 {
+    public static void main(String[] args) {
+        int a = 0;
+        int k = 0;
+        for(int i = 2; i <= 100; i++){
+            for(int j = 2; j <= i; j++){
+                a = i % j;
+                if(a == 0){
+                    k = k + 1;
+                    }
+                }
+            if(k == 1){
+                    System.out.println(i);
+                }
+                k = 0;
+            }
+        }
+    }
+
+
--- NKUST2/src/BMI.java
+++ NKUST2/src/BMI.java
@@ -1,8 +1,8 @@
-public class BMI {
+public class BMI{
     public static void main(String[] args) {
         double height = 1.75;
         int weight = 72;
-        double BMI = weight/(height * height);
+        double BMI = weight / (height * height);
         System.out.println(BMI);
     }
 
