5f0dc4856382d56269dc4b84a267501986023603
kevin1547 <c110181143@nkust.edu.tw>
2021-12-07 22:13:11 +0800

    12/7

--- Spider/spider2/src/main/java/spider2.java
+++ Spider/spider2/src/main/java/spider2.java
@@ -59,10 +59,6 @@ public class spider2 {
                                 */
 
 
-
-
-
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -83,43 +79,33 @@ public class spider2 {
                 String num = item1.getElementsByClass("board-name").get(0).text();//"Baseball";
                 String title = item1.getElementsByClass("board-title").get(0).text();
                 Document doc1 = Jsoup.connect("https://www.ptt.cc/bbs/" + num + "/index.html").get();//從board-name中選擇的素材與網頁組合並進入
-                System.out.println("Borad: " + num);
+                System.out.println("Board: " + num);
                 System.out.println("Article title: " + title);
                 Elements newsHeadlines = doc1.select("#main-container div.r-list-container.action-bar-margin.bbs-screen a");//
                 //System.out.println(doc1);
                 for (Element headline : newsHeadlines) {
-                    Elements items1 = doc1.getElementsByClass("r-ent");//從doc1中選擇r-ent
-                    for (Element item2 : items1) {//從r-ent中逐步搜尋素材
+                    //Elements items1 = doc1.getElementsByClass("r-ent");//從doc1中選擇r-ent
+                    //for (Element item2 : items1) {//從r-ent中逐步搜尋素材
                         Document doc2 = Jsoup.connect(headline.absUrl("href")).get();//進入Elements newsHeadlines所抓出的href的網址
                         //System.out.println(doc2);
                         Elements items2 = doc2.getElementsByClass("push");//從href網頁中選擇push
-                        for (Element item3 : items2) {
+                        Element item3 = items2.get(0);
                             String num2 = item3.getElementsByClass("push").get(0).getElementsByTag("span").get(2).text();//在class底下選擇第三個span(第一個留言的位置)
                             String num3 = item3.getElementsByClass("push").get(0).getElementsByTag("span").get(1).text();
                             System.out.println("Comment:" + num3 + num2);
-                            System.out.print("\n");
-                            break;
-                        }
-                        break;
-                    }
+                        //break;
+                    //}
                     break;
                 }
                 //break;
+                System.out.print("\n");
             }
 
             //break;
             //}
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("error: " + e);
         }
     }
 }
-//Elements items2 = doc1.getElementsByClass("push");
-                /*for (Element item3 : items2) {
-                    int h = 0;
-                    System.out.println(h);
-                    String num2 = item3.getElementsByClass("push").get(0).getElementsByTag("span").get(2).text();
-                    //String num2 = item3.getElementsByClass("push").get(0).getElementsByTag("span").get(2).text();
-                    System.out.println("Comment: " + num2);*/
 
